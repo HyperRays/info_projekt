dc5b958260a267e107af52deec831db3427ca88d
servrcube <63092977+servrcube@users.noreply.github.com>
2022-09-23 08:44:10 +0200

    pyo3 winit

--- graphics/src/lib.rs
+++ graphics/src/lib.rs
@@ -1,14 +1,55 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{WindowBuilder,Window}
+};
+
+#[pyclass(unsendable)]
+struct EventsHandler{
+    event_loop: Option<EventLoop<()>>,
+    window: Option<Window>
 }
 
-/// A Python module implemented in Rust.
+#[pymethods]
+impl EventsHandler{
+    #[new]
+    fn new() -> Self{
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+        Self { event_loop: Some(event_loop), window: Some(window)}
+    }
+
+    fn run(&mut self){
+        let event_loop = self.event_loop.take().unwrap();
+        let window = self.window.take().unwrap();
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            },
+            _ => {}
+        });
+    }
+}
+
 #[pymodule]
 fn graphics(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<EventsHandler>()?;
     Ok(())
 }
+
---
+++ playground/pyo3-window-test.py
@@ -0,0 +1,2 @@
+import graphics
+graphics.make_window()
